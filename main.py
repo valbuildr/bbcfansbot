2ae3aa1cfbbe8214faabcc76bd8e0cb376a5bd0b
slipinthedove <reeeniii@pm.me>
2024-03-28 23:53:58 +0000

    slashes-only > hybrids

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ async def on_ready():
     print(f"Logged in as {bot.user.name}.")
     return
 
-@bot.tree.command(name="sync")
-async def sync(interaction: discord.interaction):
-    if interaction.user.id == 1191850547138007132:
+@bot.hybrid_command(name="sync")
+async def sync(interaction: commands.Context):
+    if interaction.author.id == 1191850547138007132:
         await bot.tree.sync()
-        await interaction.response.send_message(content="Synced!")
+        await interaction.send(content="Synced!")
 
-@bot.tree.command(name="aaron", description="Sends a random picture of Aaron Heslehurst!")
-async def aaron(interaction: discord.Interaction):
+@bot.hybrid_command(name="aaron", description="Sends a random picture of Aaron Heslehurst!")
+async def aaron(interaction: commands.Context):
     no = random.randint(0, 4)
     match no:
         case 2:
@@ -25,6 +25,6 @@ async def aaron(interaction: discord.Interaction):
         case _:
             fileformat = "jpg"
     image = discord.File(f"images/aaron/{no + 1}.{fileformat}")
-    await interaction.response.send_message(file=image)
+    await interaction.send(file=image)
 
 bot.run(config.token)
