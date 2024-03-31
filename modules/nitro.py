aa976488d0ee1b75b963fdbe8f727b9e99f97763
slipinthedove <reeeniii@pm.me>
2024-03-31 16:08:05 -0300

    add SIDs to a database rather than formatting in-code
    also helps getting things started for db-related commands

--- modules/nitro.py
+++ modules/nitro.py
@@ -37,44 +37,16 @@ async def verify_date(date):
         return "Incorrect Date"
 
  
-async def resolve_sid(sid):
-    match sid:
-        case "BBC News [World]": parsedsid = "bbc_world_service"
-        case "BBC News [UK]": parsedsid = "bbc_news24"
-        case "BBC One Scotland": parsedsid = "bbc_one_scotland"
-        case "BBC One North East": parsedsid = "bbc_one_north_east"
-        case "BBC One North West": parsedsid = "bbc_one_north_west"
-        case "BBC One East Midlands": parsedsid = "bbc_one_east_midlands"
-        case "BBC One West Midlands": parsedsid = "bbc_one_west_midlands"
-        case "BBC One East Yorkshire": parsedsid = "bbc_one_east_yorkshire"
-        case "BBC One London": parsedsid = "bbc_one_london"
-        case "BBC One South East": parsedsid = "bbc_one_south_east"
-        case "BBC One South West": parsedsid = "bbc_one_south_west"
-        case "BBC One NI": parsedsid = "bbc_one_northern_ireland"
-        case "BBC One Wales": parsedsid = "bbc_one_wales"
-        case "BBC One West": parsedsid = "bbc_one_west"
-        case "BBC One East": parsedsid = "bbc_one_east"
-        case "BBC One South": parsedsid = "bbc_one_south"
-        case "BBC One Yorks": parsedsid = "bbc_one_yorks"
-        # case "BBC One Wales": parsedsid = "bbc_one_wales" # duplicate
-        case "BBC One": parsedsid = "bbc_one_hd"
-        case "BBC Two England": parsedsid = "bbc_two_england"
-        case "BBC Two Scotland": parsedsid = "bbc_two_scotland"
-        case "BBC Two NI": parsedsid = "bbc_two_northern_ireland_digital"
-        case "BBC Two Wales": parsedsid = "bbc_two_wales_digital"
-        case "BBC Two": parsedsid = "bbc_two_hd"
-        case "BBC Three": parsedsid = "bbc_three_hd"
-        case "BBC Four": parsedsid = "bbc_four_hd"
-        case "CBeebies": parsedsid = "cbeebies_hd"
-        case "CBBC": parsedsid = "cbbc_hd"
-        case "BBC Parliament": parsedsid = "bbc_parliament"
-        case "BBC Alba": parsedsid = "bbc_alba_hd"
-        case "BBC Scotland": parsedsid = "bbc_scotland_hd"
-        case _: parsedsid = "bbc_news24" # fall back to news channel if invalid sid is passed. people are dumb.
+async def resolve_sid(sid, db):
+    for val in db['NitroSIDs']:
+        if sid == val:
+            parsedsid = db['NitroSIDs'][val]
+        else:
+            continue
     return parsedsid
 
 
-async def get_schedule(date=None, sid="BBC News [UK]", page=1):
+async def get_schedule(db, sid, date=None, page=0):
     if not isinstance(date, datetime.datetime):
         # goes under a check to see if the inputted values are correct
         if not date: 
@@ -85,7 +57,7 @@ async def get_schedule(date=None, sid="BBC News [UK]", page=1):
     if isinstance(parsed_date, str):
         raise Exception(f"ERROR - {parsed_date}")
     else:
-        parsedsid = await resolve_sid(sid)
+        parsedsid = await resolve_sid(sid, db)
         if not isinstance(parsedsid, str): raise Exception("ERROR - IncorrectSID") # raise error if sid given is incorrect
         # mixin titles is needed to get the proper related info about the scheduled broadcast's naming.
         params = { 
@@ -105,7 +77,6 @@ async def get_schedule(date=None, sid="BBC News [UK]", page=1):
                     if j['nitro']['results']['total'] > 0:
                         # gives the parsed sid information, and the date, alongside with the items.
                         listing = { 
-                            "sid": parsedsid, 
                             "passedSid": sid,
                             "date": parsed_date.strftime("%Y-%m-%d"),
                             "items": [] 
