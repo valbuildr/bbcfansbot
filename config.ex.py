c3e55ad41105dfd79ef19d06dadd248d83cd95dc
valentine <150450259+valbuildr@users.noreply.github.com>
2024-04-07 17:25:39 +0000

    news fans bot autothread and publishing to other announcement channels

--- config.ex.py
+++ config.ex.py
@@ -1,4 +1,5 @@
 main_discord_token = "" # discord bot token
 helper_discord_token = "" # bot token for news fans helper
+nf_announcement_webhook_url = "" # webhook url for news fans updates
 
 nitro_secret = "" # apikey for nitro
--- news_fans_helper.py
+++ news_fans_helper.py
@@ -1,5 +1,6 @@
-import discord, logging
+import discord, logging, traceback
 from discord.ext import commands
+from messageutils import autothread_from_message
 import config
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
@@ -14,8 +15,20 @@ async def on_ready():
 async def ping(ctx: commands.Context):
     await ctx.send(content=f"My ping is {round(bot.latency * 1000)}ms!")
 
+async def publish_to_announcement_channel(message: discord.Message):
+    if message.channel.id == 1226576062675419226: # log
+        send_to = discord.Webhook.from_url(url=config.nf_announcement_webhook_url, client=bot)
+
+        files = []
+        for attachment in message.attachments:
+            await attachment.to_file()
+
+        await send_to.send(content=message.content, files=files, embeds=message.embeds)
+
 @bot.event
 async def on_message(message: discord.Message):
+    await autothread_from_message(message)
+    await publish_to_announcement_channel(message)
     await bot.process_commands(message)
 
 bot.run(config.helper_discord_token)
