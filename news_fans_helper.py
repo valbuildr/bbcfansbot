3ca83e5bf3323596b8609dff4bff6c53d906a3ee
valentine <150450259+valbuildr@users.noreply.github.com>
2024-04-07 16:54:28 +0000

    news fans bot log + ready response

--- news_fans_helper.py
+++ news_fans_helper.py
@@ -1,11 +1,21 @@
-import discord
+import discord, logging
 from discord.ext import commands
 import config
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
+newsfansbotlog = logging.getLogger('discord.newsfansbot')
+
+@bot.event
+async def on_ready():
+    newsfansbotlog.info(f'Logged in as {bot.user.name}.')
+
 @bot.command()
 async def ping(ctx: commands.Context):
-    await ctx.send(content=f"My ping is {bot.latency}!")
+    await ctx.send(content=f"My ping is {round(bot.latency * 1000)}ms!")
+
+@bot.event
+async def on_message(message: discord.Message):
+    await bot.process_commands(message)
 
 bot.run(config.helper_discord_token)
