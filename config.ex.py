5d549cc57978358f06312cfb469bd979800e669e
valentine <v4lbuild3r@gmail.com>
2024-04-11 22:43:34 -0500

    move planning helper to bbcnewsfans/planning-helper

--- config.ex.py
+++ config.ex.py
@@ -1,5 +1,24 @@
-main_discord_token = "" # discord bot token
-helper_discord_token = "" # bot token for news fans helper
-nf_announcement_webhook_url = "" # webhook url for news fans updates
+class BBCFansBot:
+    token = ""
 
-nitro_secret = "" # apikey for nitro
+class NewsFansHelper:
+    token = ""
+
+    class AnnouncementRelay:
+        from_server_id = 0
+        from_channel_id = 0
+
+        to_webhook_url = ""
+
+class Nitro:
+    secret = ""
+
+
+
+# dont edit these
+# these are only for backwards compatability
+
+main_discord_token = BBCFansBot.token
+helper_discord_token = NewsFansHelper.token
+nf_announcement_webhook_url = NewsFansHelper.AnnouncementRelay.to_webhook_url
+nitro_secret = Nitro.secret
